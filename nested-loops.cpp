9ee880ac23944551c08cc2c5c2c8feaefb0afe08
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-19 12:05:13 +0200

    Add files via upload

---
+++ nested-loops.cpp
@@ -0,0 +1,94 @@
+/*
+
+        // NESTED FOR LOOP NUMBERS FROM 0 TO 100 REPEATED TWICE CODE 
+
+    #include <iostream>
+    #include <string>
+
+    int main()
+{
+    int number;
+    for (int i = 1; i <= 2; i++) {
+        for (int number = 0; number <= 100; number++) {
+            std::cout << number << ' ';
+    }
+        std::cout << '\n';
+    }
+
+    return 0;
+
+
+}
+
+*/
+
+/*
+
+        // NESTED FOR LOOP NUMBERS FROM 0 TO 500 REPEATED FIVE TIMES CODE
+
+    int main() {
+
+    int number;
+
+    for (int i = 1; i <= 5; i++) {
+        for (int number = 0; number <= 50;number++) {
+            std::cout << number << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
+
+*/
+
+/*
+
+        // NESTED FOR LOOP NUMBERS FROM 0 TO 5 REPEATED TEN TIMES CODE
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+
+    int number;
+
+    for (int i = 1; i <= 10; i++) {
+        for (int number = 0; number <= 5; number++) {
+            std::cout << number << ' ';
+        }
+        std::cout << '\n';
+    }
+
+    return 0;
+}
+
+*/
+
+/*
+
+        // FOR WHILE NESTED LOOP CODE 
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+
+    int rows = 5;
+    int columns = 5;
+
+    for (int i = 1; i <= rows; ++i) {
+        int j = 1;
+
+        while (j <= columns) {
+            std::cout << i * j << "\t";
+            ++j;
+        }
+
+        std::cout << '\n';
+    }
+
+    return 0;
+}
+
+*/
